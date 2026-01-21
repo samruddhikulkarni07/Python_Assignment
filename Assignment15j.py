83f76aac43f58ce51b7e50d79157a7917b0cfa68
Samruddhi Santosh Kulkarni <sk5835527@gmail.com>
2026-01-21 00:19:20 -0800

    Add files via upload

---
+++ Assignment15j.py
@@ -0,0 +1,24 @@
+def main():
+    Size = 0
+    Value = 0
+
+    Size = int(input("Enter number of elements:"))
+
+    Data = list()
+
+    print("Enter elements:")
+
+    for i in range(Size):
+        Value = int(input())
+        Data.append(Value)
+
+    print("Actual data is:",Data)
+
+    FData = list(filter(lambda No:(No%2==0),Data))
+    print("Data after filter is:",FData)
+
+    print("Count of even numbers:",len(FData))
+
+
+if __name__ == "__main__":
+    main()
